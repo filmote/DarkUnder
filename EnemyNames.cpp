44fda9d56beb19436c4d42c38d467b213096e715
Pharap <pharap@hotmail.com>
2017-10-20 09:46:37 +0100

    Corrected Wraith's name
    Oops, that bug was my fault.

--- EnemyNames.cpp
+++ EnemyNames.cpp
@@ -5,7 +5,7 @@
 const char BeholderName[] PROGMEM = "A BEHOLDER";
 const char SkeletonName[] PROGMEM = "A SKELETON";
 const char DisplacerName[] PROGMEM = "A DISPLACER";
-const char WraithName[] PROGMEM = "A DISPLACER";
+const char WraithName[] PROGMEM = "A WRAITH";
 const char DragonName[] PROGMEM = "A DRAGON";
 const char RatName[] PROGMEM = "A RAT";
 const char SlimeName[] PROGMEM = "A SLIME";
