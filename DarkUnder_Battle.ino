90e990775bdaeddeca8239574c7be20da21baa8c
Pharap <2933055+Pharap@users.noreply.github.com>
2017-11-28 23:30:04 +0000

    Fix hp not reaching zero (-70)

--- DarkUnder_Battle.ino
+++ DarkUnder_Battle.ino
@@ -1,26 +1,26 @@
 #include <Arduboy2.h>
 
 /* -----------------------------------------------------------------------------------------------------------------------------
- *  Battle Loop  
- *  
+ *  Battle Loop
+ *
  *  GameStates:
- *  
+ *
  *  Battle_EnemyAttacks_Init -    Displays the initial '{enemy} attacks !' message and prepares for and enemy attck.
- *  Battle_EnemyAttacks -         Displays the dice animation and inflicts damage to the player.  
+ *  Battle_EnemyAttacks -         Displays the dice animation and inflicts damage to the player.
  *  Battle_PlayerDecides -        Presents the players battle options including attack, defend, cast a spell ..
  *  Battle_PlayerAttacks -        Throws the dice and inflicts damage on the enemy.
  *  Battle_PlayerDefends -        Inflicts 1 point of damage on the enemy and randomly gains player hit points.
  *  Battle_PlayerCastsSpell -     Inflicts 5 points of damage on the enemy.
  *  Battle_EnemyDies -            Handles and end of battel where the enemy dies.
- * 
+ *
  *  Returns delay in milliseconds to wait.
- *  
+ *
  * -----------------------------------------------------------------------------------------------------------------------------
  */
 uint16_t battleLoop() {
-  
+
   uint16_t delayLength = FIGHT_DELAY;
-  
+
   drawPlayerVision(&myHero, &myLevel);
   Sprites::drawSelfMasked(DIRECTION_X_OFFSET, DIRECTION_Y_OFFSET, fight_icon, 0);
 
@@ -28,7 +28,7 @@ uint16_t battleLoop() {
 
   switch (gameState) {
 
-    
+
     case GameState::Battle_EnemyAttacks_Init:  // ----------------------------------------------------------------------------------------------------------------------------------
       font3x5.print(getEnemyName(enemies[attackingEnemyIdx].getEnemyType()));
       font3x5.print(F("\nATTACKS!"));
@@ -40,19 +40,19 @@ uint16_t battleLoop() {
       {
         arduboy.drawCompressed(12, 12, fight_scratch_Mask, BLACK);
         arduboy.drawCompressed(12, 12, fight_scratch, WHITE);
-  
+
         uint8_t hpLoss = random(0, ENEMY_MAX_ATTACK);
         font3x5.print(F("YOU TAKE\n"));
         font3x5.print(hpLoss);
         font3x5.print(F(" DAMAGE!"));
         font3x5.setCursor(33, 26);
         font3x5.print(hpLoss);
-        myHero.setHitPoints(myHero.getHitPoints() > hpLoss ? myHero.getHitPoints() - hpLoss : 0); 
+        myHero.setHitPoints(myHero.getHitPoints() > hpLoss ? myHero.getHitPoints() - hpLoss : 0);
         gameState = GameState::Battle_PlayerDecides;
 
         if (myHero.getHitPoints() == 0)  gameState = GameState::Battle_PlayerDies;
       }
-      
+
       break;
 
 
@@ -93,11 +93,11 @@ uint16_t battleLoop() {
         if (fightButtons[(uint8_t)FightButtons::Magic])    { arduboy.drawCompressed(102, 44, fight_actions_3, WHITE); }
         if (fightButtons[(uint8_t)FightButtons::Potion])   { arduboy.drawCompressed(113, 44, fight_actions_4, WHITE); }
         Sprites::drawSelfMasked(81 + (((uint8_t)fightButton) * 11), 56, icnSelect, 0);
-        
-        if ((buttons & LEFT_BUTTON_MASK) && (uint8_t)fightButton > 0) { 
-          
+
+        if ((buttons & LEFT_BUTTON_MASK) && (uint8_t)fightButton > 0) {
+
           for (uint8_t i = (uint8_t)fightButton - 1; i >= 0; --i) {
-            if (fightButtons[i]) { 
+            if (fightButtons[i]) {
               fightButton = (FightButtons)i;
               break;
             }
@@ -106,21 +106,21 @@ uint16_t battleLoop() {
         }
 
         else if (buttons & RIGHT_BUTTON_MASK) {
-          
+
           for (uint8_t i = (uint8_t)fightButton + 1; i < (uint8_t)FightButtons::Count; ++i) {
-            if (fightButtons[i]) { 
+            if (fightButtons[i]) {
               fightButton = (FightButtons)i;
               break;
             }
           }
-          
+
         }
 
         else if (buttons & BACK_BUTTON_MASK)  {
           savedState = gameState;
           gameState = GameState::InventorySelect;
         }
-          
+
         else if (buttons & SELECT_BUTTON_MASK)  {
 
           switch (fightButton) {
@@ -146,11 +146,11 @@ uint16_t battleLoop() {
 
             default:
               break;
-          
+
           }
-        
+
         }
-        
+
       }
 
       delayLength = 0;
@@ -161,38 +161,32 @@ uint16_t battleLoop() {
       {
         arduboy.drawCompressed(19, 19, fight_hero_strike_Mask, BLACK);
         arduboy.drawCompressed(19, 19, fight_hero_strike, WHITE);
-        
+
         uint8_t hpLoss = random(1, myHero.getAttackPower() + 1);
         font3x5.print(F("YOU DEAL\n"));
         font3x5.print(hpLoss);
         font3x5.print(F(" DAMAGE!\n"));
         font3x5.setCursor(32, 24);
         font3x5.print(hpLoss);
-        if (enemies[attackingEnemyIdx].getHitPoints() > hpLoss) {
-            enemies[attackingEnemyIdx].setHitPoints(enemies[attackingEnemyIdx].getHitPoints() - hpLoss);
-        }
-        else {
-            enemies[attackingEnemyIdx].setHitPoints(0);
-            enemies[attackingEnemyIdx].setEnabled(false);
-        }
+        damageEnemy(attackingEnemyIdx, hpLoss);
 
         gameState = GameState::Battle_EnemyDies;
         if (enemies[attackingEnemyIdx].getEnabled()) { gameState = GameState::Battle_EnemyAttacks_Init; }
       }
 
-      break; 
+      break;
 
 
     case GameState::Battle_PlayerDefends:  // ---------------------------------------------------------------------------------------------------------------------------------------
-    
+
       arduboy.drawCompressed(12, 15, fight_hero_shield_Mask, BLACK);
       arduboy.drawCompressed(12, 15, fight_hero_shield, WHITE);
- 
+
       {
         uint8_t maxHP = enemies[attackingEnemyIdx].getAttackPower() - myHero.getDefence();
         if (maxHP < 2) maxHP = 2;
         if (maxHP > 10) maxHP = 10;
-        uint8_t hpLoss = random(0, maxHP);   
+        uint8_t hpLoss = random(0, maxHP);
 
         font3x5.print(F("TAKE "));
         font3x5.print(hpLoss);
@@ -202,33 +196,45 @@ uint16_t battleLoop() {
         font3x5.print(hpLoss);
 
         myHero.setHitPoints(myHero.getHitPoints() + hpLoss);
-        if (enemies[attackingEnemyIdx].getHitPoints() > 1) { enemies[attackingEnemyIdx].setHitPoints(enemies[attackingEnemyIdx].getHitPoints() - 1); } else { enemies[attackingEnemyIdx].setEnabled(false); }
-        
+        damageEnemy(attackingEnemyIdx, 1);
+
         gameState = GameState::Battle_EnemyDies;
         if (enemies[attackingEnemyIdx].getEnabled()) gameState = GameState::Battle_PlayerDecides;
       }
 
-      break;   
+      break;
 
 
     case GameState::Battle_PlayerCastsSpell:  // ---------------------------------------------------------------------------------------------------------------------------------------
-    
-      arduboy.drawCompressed(12, 15, fight_hero_spell_Mask, BLACK);
-      arduboy.drawCompressed(12, 15, fight_hero_spell, WHITE);
 
-      if (enemies[attackingEnemyIdx].getHitPoints() > PLAYER_CASTS_SPELL) { enemies[attackingEnemyIdx].setHitPoints(enemies[attackingEnemyIdx].getHitPoints() - PLAYER_CASTS_SPELL); } else { enemies[attackingEnemyIdx].setEnabled(false); }
-      myHero.setInventory(myHero.getSlotNumber(ItemType::Scroll), ItemType::None);
+        arduboy.drawCompressed(12, 15, fight_hero_spell_Mask, BLACK);
+        arduboy.drawCompressed(12, 15, fight_hero_spell, WHITE);
 
-      gameState = GameState::Battle_EnemyDies;
-      if (enemies[attackingEnemyIdx].getEnabled()) gameState = GameState::Battle_EnemyAttacks_Init;
+        damageEnemy(attackingEnemyIdx, PLAYER_CASTS_SPELL);
+        myHero.setInventory(myHero.getSlotNumber(ItemType::Scroll), ItemType::None);
 
-      break;
+        gameState = GameState::Battle_EnemyDies;
+        if (enemies[attackingEnemyIdx].getEnabled()) gameState = GameState::Battle_EnemyAttacks_Init;
+
+        break;
 
     default: break;
-      
+
   }
 
   drawEnemyHitPointsBar(enemies[attackingEnemyIdx].getHitPoints(), enemies[attackingEnemyIdx].getHitPointsMax());
   return delayLength;
+
+}
+
+void damageEnemy(uint8_t attackingEnemyIdx, uint8_t hpLoss) {
+
+    if (enemies[attackingEnemyIdx].getHitPoints() > hpLoss) {
+        enemies[attackingEnemyIdx].setHitPoints(enemies[attackingEnemyIdx].getHitPoints() - hpLoss);
+    }
+    else {
+        enemies[attackingEnemyIdx].setHitPoints(0);
+        enemies[attackingEnemyIdx].setEnabled(false);
+    }
 
 }
